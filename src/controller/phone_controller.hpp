08fe872066bb6c53c582db99de736dc88ea71c35
AnarLabs <anardevstudio@gmail.com>
2026-01-26 15:56:49 +0400

    Dump commit

--- src/controller/phone_controller.hpp
+++ src/controller/phone_controller.hpp
@@ -5,4 +5,4 @@
 
 void registerSamsungRoute(crow::Crow<>& app, PhoneBrandEndpoints& phoneEndpoints);
 void registerAppleRoute(crow::Crow<>& app, PhoneBrandEndpoints& phoneEndpoints);
-void registerXiaomiRoute(crow::Crow<>& app, PhoneBrandEndpoints& phoneEndpoints);
+void registerXiaomiRoute(crow::Crow<>& app, PhoneBrandEndpoints& phoneEndpoints);  //Selamma hahahahahah
